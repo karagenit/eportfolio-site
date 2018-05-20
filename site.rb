ffc3c4bfdf9149320cad47f268a84aeb9133e238
Caleb Smith <karagenit@outlook.com>
2018-05-20 19:03:49 -0400

    Parse CSV with Headers

--- site.rb
+++ site.rb
@@ -5,7 +5,7 @@ require 'csv'
 
 get '/' do
   redirect '/error' unless File.file?('./eportfolio/output.csv')
-  erb :index, locals: { data: CSV.parse(IO.read('./eportfolio/output.csv')) }
+  erb :index, locals: { data: CSV.parse(IO.read('./eportfolio/output.csv'), headers: true) }
 end
 
 get '/error' do
